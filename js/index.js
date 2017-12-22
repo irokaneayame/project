04d986dcddff9d84b64347d17072bb9623527dfa
Ayame <915658264@qq.com>
2017-12-22 08:30:08 +0800

    new

--- js/index.js
+++ js/index.js
@@ -1,12 +1,42 @@
 $(function() {
-	$('.xiala').hover(function(){
+	$('.xiala').hover(function() {
 		$(this).find('ul').stop().slideDown()
-	},function(){
+	}, function() {
 		$(this).find('ul').stop().slideUp()
 	})
 	$('.navList').hover(function() {
-		$(this).find('.list').stop().slideDown()
+		$(this).find('.list').stop().slideDown();
 	}, function() {
-		$(this).find('.list').stop().slideUp()
+		$(this).find('.list').stop().slideUp();
 	})
+	$("#qiang span").click(function() {
+		$(window).scrollTop($(this).parent().parent().parent().parent().offset().top);
+		$(this).addClass("select").siblings().removeClass("select");
+		if($(this).index() == 0) {
+			foo()
+		} else {
+			$.get("../json/index.json", function(data) {
+				var html = "";
+				$.each(data, function(key, value) {
+					html += '<li><a href=""><img src="' + value.src1 + '"></a><div></div></li>'
+				});
+				$("#navlist").html(html);
+			})
+		}
+	})
+	foo();
+	function foo() {
+		$.get("../json/index.json", function(data) {
+			var html = "";
+			$.each(data, function(key, value) {
+				html += '<li><a href=""><img src="' + value.src + '"></a><div></div></li>'
+			});
+			$("#navlist").html(html);
+		})
+	}
+	/*$('.bg').hover(function(){
+		$(this).parent().find(".dan").show()
+	},function(){
+		$(this).parent().find(".dan").hide()
+	})*/
 })
